d91efd1e7fce2ea1a3df9c6618bfcda7653ed2b9
LUCAS FERREIRA RODRIGUES DE MORAIS <lucasferreirarm@gmail.com>
2022-12-07 10:14:46 -0300

    Teste

--- server.js
+++ server.js
@@ -1,24 +1,3 @@
-// See https://github.com/typicode/json-server#module
-// const jsonServer = require('json-server')
-// const server = jsonServer.create()
-// const router = jsonServer.router('db.json')
-// const middlewares = jsonServer.defaults()
-
-// server.use(middlewares)
-// // Add this before server.use(router)
-// server.use(jsonServer.rewriter({
-//     '/api/*': '/$1',
-//     '/blog/:resource/:id/show': '/:resource/:id'
-// }))
-// server.use(router)
-// server.listen(3000, () => {
-//     console.log('JSON Server is running')
-// })
-
-// // Export the Server API
-// module.exports = server
-
-
 
 const jsonServer = require('json-server')
 const clone = require('clone')
@@ -26,7 +5,6 @@ const data = require('./db.json')
 
 const server = jsonServer.create()
 
-// For mocking the POST request, POST request won't make any changes to the DB in production environment
 const router = jsonServer.router(clone(data))
 const middlewares = jsonServer.defaults()
 
